70738fb3dd0fa7416e00c808e54a324417d1ebd2
Dmitry Nechaev <fruitpizza49@gmail.com>
2017-09-22 15:11:28 +0300

    Deleted trash. Now user can make triangle of any size he wants.

--- pascal.cpp
+++ pascal.cpp
@@ -5,18 +5,12 @@ using namespace std;
 
 int main()
 {
-    // TODO: написать вывод треугольника Паскаля с использованием vector.
-    //
-    // Вид треугольника:
-    // 1
-    // 1 1
-    // 1 2 1
-    // 1 3 3 1
-    // 1 4 6 4 1
-    // ...
     vector<int> v;
     int size;
-    for (int i=0; i<5; i++)
+    int n;
+    cout<<"Enter length of triangle"<<endl;
+    cin>>n;
+    for (int i=0; i<n; i++)
         {size=v.size();
 	v.push_back(1);
 	if (i>=2)
@@ -26,9 +20,9 @@ int main()
 	v.push_back(1);    
 	}
     int k=0;
-    for (int i=0; i<5; i++){
+    for (int i=0; i<n; i++){
 	for (int j=0; j<=i; j++)
-	    {cout<<v[k];
+	    {cout<<v[k]<<" ";
 	    k++;}
 	    cout<<endl;}	    
     return 0;
