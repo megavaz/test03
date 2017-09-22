f87747d9d6bdb72e832312e6b9bdd433c7a5909f
Dmitry Nechaev <fruitpizza49@gmail.com>
2017-09-22 14:45:20 +0300

    Made this program :D

--- pascal.cpp
+++ pascal.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -13,6 +14,23 @@ int main()
     // 1 3 3 1
     // 1 4 6 4 1
     // ...
+    vector<int> v;
+    int size;
+    for (int i=0; i<5; i++)
+        {size=v.size();
+	v.push_back(1);
+	if (i>=2)
+	for (int j=i;j>1;j--)
+	    {v.push_back(v[size-j]+v[size-j+1]);}
+	if (i>=1)
+	v.push_back(1);    
+	}
+    int k=0;
+    for (int i=0; i<5; i++){
+	for (int j=0; j<=i; j++)
+	    {cout<<v[k];
+	    k++;}
+	    cout<<endl;}	    
     return 0;
 }
 
